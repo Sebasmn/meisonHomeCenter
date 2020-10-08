b8c2f5623be4aa0a6f82dabab058feba9a68d2be
Sebastian Mayorga <sebasmayorga98@gmail.com>
2020-10-08 10:03:41 -0500

    avanceSebitas

--- footer.php
+++ footer.php
@@ -53,16 +53,13 @@
 
   <div class="mr-md-auto text-center text-md-left">
     <div class="copyright">
-      &copy; Copyright <strong><span>Menison Homecenter</span></strong>. All Rights Reserved
+    Copyright &copy; 2020 <strong><span>Meison Homecenter</span></strong>
     </div>
     
   </div>
   <div class="social-links text-center text-md-right pt-3 pt-md-0">
-    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
     <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
     <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
   </div>
 </div>
 </footer>
