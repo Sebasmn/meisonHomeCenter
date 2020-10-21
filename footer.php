662825ffa27b86a4b7d8aeacbd8971e70651ac10
grubira4167 <grubira4167@uta.edu.ec>
2020-10-21 08:59:04 -0500

    direccion

--- footer.php
+++ footer.php
@@ -7,10 +7,9 @@
       <div class="col-lg-3 col-md-6 footer-contact">
         <h3>Mentor</h3>
         <p>
-          A108 Adam Street <br>
-          New York, NY 535022<br>
-          United States <br><br>
-          <strong>Phone:</strong> +1 5589 55488 55<br>
+          Av. Victor Hugo <br>
+          y Batalla de Pichincha<br><br>
+          <strong>Teléfono:</strong> 0980582796<br>
           <strong>Email:</strong> info@example.com<br>
         </p>
       </div>
